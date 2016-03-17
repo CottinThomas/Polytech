3707b23fa06100aaeb1318f5c98cd7ca89ded8b1
CottinThomas <thomas_cottin@yahoo.fr>
2016-03-17 16:59:53 +0100

    End TP1 SE / Init TP2

--- SE/TP1/exo6_fork.c
+++ SE/TP1/exo6_fork.c
@@ -14,23 +14,29 @@ int main (int argc, char *argv[]){
 		exit(1);
 	}
 	else if(pidfils1 == 0){
-		printf("Je suis %d et je vais tuer mon pere !\n",getpid());
+		sleep(1);
+		printf("[%d] et je vais tuer mon pere !\n",getpid());
 		kill(getppid(),SIGTERM);
+		printf("[%d] AHAHA ! IL EST MOOOOOOORT !!!!\n",getpid());
+		while(1);
 	}
 	else{
-		printf("Premier fils créé, pid = %d\n",pidfils1);
+		printf("[%d] J'ai un fils ! Il s'appel %d\n",getpid(),pidfils1);
 		pidfils2 = fork();
 		if(pidfils2 == -1){
 			printf("FORK FAILED\n");
 			exit(2);
 		}
 		else if(pidfils2 == 0){
-			printf("Je suis %d et je vais tuer mon frere !\n",getpid());
+			sleep(5);
+			printf("[%d] NOOOOOON ! Vangeance ! Je vais tuer mon frere !\n",getpid());
 			kill(pidfils1, SIGTERM);
-
+			printf("[%d] %d est mort ! Il ne me reste plus qu'a me suicider ! ",getpid(),pidfils1);
 		}
 		else{
-			printf("Second fils créé; pid = %d\n",pidfils2);
+			printf("[%d] J'ai de nouveau un fils ! Il s'appel %d\n",getpid(),pidfils2);
+			while(1);
 		}
 	}
+	exit(0);
 }
--- SE/TP1/exo6_thread.c
+++ SE/TP1/exo6_thread.c
@@ -2,17 +2,52 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
 
-void *killproc(pid_t pid){
-	printf("Je suis %d, je vais tuer %d\n", getpid(), pid );
-	kill(pidfils1, SIGTERM);
+
+
+void *premier_fils(void* arg){
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	pthread_setcancelstate(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+	pthread_t pere= *((pthread_t*)arg);
+	sleep(5);
+	printf("Je suis %d\n", (int)pthread_self());
+	printf("Je tue mon pere !\n");
+	pthread_cancel(pere);
+	printf("KILLED\n");
+	return 0;
+}
+
+void *second_fils(void* arg){
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	pthread_setcancelstate(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+	pthread_t fils1= *((pthread_t*)arg);
+	sleep(5);
+	printf("Je suis %d\n", (int)pthread_self());
+	printf("Je tue mon frere !\n");
+	pthread_cancel(fils1);
+	printf("KILLED\n");
+	return 0;
+}
+
+void *familly(void* args){
+	printf("Création des fils\n");
+	pthread_t* mesFils = (pthread_t*) args;
+	pthread_t moi = pthread_self();
+	pthread_create(&(mesFils[0]),NULL,premier_fils,(void*)&moi);
+	sleep(2);
+	pthread_create(&(mesFils[1]),NULL,second_fils,(void*)&mesFils[0]);
+	printf("Moi, pere %d, j'ai créé %d et %d\n",(int)pthread_self(), (int)mesFils[0], (int)mesFils[1]);
+	return 0;
 }
 
 int main (int argc, char *argv[]){
-	printf("Je suis le père, pid %d", gepid()); 
-	pthread_t fils1, fils2;
-	pthread_create(&fils1,NULL,killproc,getpid());
-	pthread_create(&fils2,NULL,killproc,fils1);
-
-
+	pthread_t pere;
+	pthread_t fils[2];
+	pthread_create(&pere,NULL,familly,(void*)&fils);
+	pthread_join(pere, NULL);
+	pthread_join(fils[0], NULL);
+	pthread_join(fils[1],NULL);
+	sleep(3);
+	exit(0);
 }
